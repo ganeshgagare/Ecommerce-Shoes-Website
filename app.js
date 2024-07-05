3641539894a3535fecce156ae127963d030cece8
GANESH GAGARE <ganesh.gagare.19@gmail.com>
2024-07-05 10:57:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Sports Shoes",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/sports.png",
+      },
+      {
+        code: "blue",
+        img: "./img/sports2.png",
+        
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Formal Shoes",
+    price: 149,
+    colors: [
+      {
+        code: "black",
+        img: "./img/formal.png",
+      },
+      {
+        code: "brown",
+        img: "./img/formal2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Casual Shoes",
+    price: 109,
+    colors: [
+      {
+        code: "yellow",
+        img: "./img/casual.png",
+      },
+      {
+        code: "green",
+        img: "./img/casual2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Sneakers",
+    price: 129,
+    colors: [
+      {
+        code: "blue",
+        img: "./img/sneakers.png",
+      },
+      {
+        code: "red",
+        img: "./img/sneakers2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Flip Flops",
+    price: 99,
+    colors: [
+      {
+        code: "blue",
+        img: "./img/flipflops.png",
+      },
+      {
+        code: "purple",
+        img: "./img/flipflops2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "₹" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
